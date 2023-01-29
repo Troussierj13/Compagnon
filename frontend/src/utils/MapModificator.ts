cb6619ebeaa4bbdc4cc9af02857592fc5578ec85
Troussierj13 <troussier.julien@gmail.com>
2023-01-29 20:18:55 +0100

    Finish MapModificator Add Jest Create unit test for MapModificator

--- frontend/src/utils/MapModificator.ts
+++ frontend/src/utils/MapModificator.ts
@@ -1,33 +1,25 @@
-type ModificatorPriority = '1' | '10' | '100' | '1000';
-
+type ModificatorOperation = '+' | '*'
 // eslint-disable-next-line no-unused-vars
-type FMod = (attr: number) => number;
-
-type ModificatorParam = {
-    priority: ModificatorPriority;
-    fMod: FMod;
+export type ModificatorParam = {
+    mod: number;
+    op: ModificatorOperation;
 }
+
 type Modificators = {
-    1: Array<FMod>;
-    10: Array<FMod>;
-    100: Array<FMod>;
-    1000: Array<FMod>;
+    '+': Array<number>;
+    '*': Array<number>;
 }
 
 export class MapModificator {
-    private id: string
     private value: number
     private modificators: Modificators
 
-    constructor(id: string, value: number) {
-        this.id = id
+    constructor(value: number) {
         this.value = value
         this._modifiedValue = value
         this.modificators = {
-            1: [],
-            10: [],
-            100: [],
-            1000: [],
+            '+': [0],
+            '*': [1]
         }
     }
 
@@ -38,14 +30,12 @@ export class MapModificator {
     }
 
     public addModificator(mod: ModificatorParam) {
-        this.modificators[mod.priority as ModificatorPriority].push(mod.fMod)
+        this.modificators[mod.op].push(mod.mod)
 
         this.calculate()
-
-        console.log(this.modificators)
     }
 
     private calculate() {
-        this._modifiedValue = 0
+        this._modifiedValue = (this.value + this.modificators['+'].reduce((acc, curr) => acc + curr)) * this.modificators['*'].reduce((acc, curr) => acc * curr)
     }
 }
