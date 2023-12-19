85705df45dc49dc74b7cd2705d2a212231e58b4d
Troussierj13 <troussier.julien@gmail.com>
2023-12-19 20:20:23 +0100

    Revert "Add Tauri"
    This reverts commit 897af5c66c2443382386e40d8959f130eb6a2d10.

--- frontend/src/views/EdgeDisplaySystem.vue
+++ frontend/src/views/EdgeDisplaySystem.vue
@@ -27,6 +27,7 @@
                 <div
                     v-for="playerId in state.elemLeft"
                     :key="playerId"
+                    :class="{ 'translate-x-72': state.currentId === playerId }"
                     class="element-left"
                     @click="onClickPlayer(playerId)">
                     <div class="ml-auto mr-12 my-auto h-fit">
@@ -34,6 +35,7 @@
                     </div>
                 </div>
                 <div
+                    :class="{ 'translate-x-72': state.hoverLeft, 'translate-x-52': props.players.filter(el => !state.elemLeft.includes(el._id) && !state.elemRight.includes(el._id)).length > 0 }"
                     class="element-left"
                     @click="TryHoverLeft"
                 >
@@ -46,6 +48,38 @@
                             :key="player._id"
                             class="w-full rounded hover:bg-slate-200"
                             @click="state.elemLeft.push(player._id)">
+                            <span class="ml-2">{{ player.name }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="vertical-container right-0">
+            <div class="flex flex-col justify-center gap-6 w-full h-full">
+                <div
+                    v-for="playerId in state.elemRight"
+                    :key="playerId"
+                    :class="{ '-translate-x-72': state.currentId === playerId }"
+                    class="element-right group"
+                    @click="state.currentId = playerId">
+                    <div class="mr-auto ml-12 my-auto h-fit">
+                        {{ props.players.filter(pl => pl._id === playerId)[0].name }}
+                    </div>
+                </div>
+                <div
+                    :class="{ '-translate-x-20': state.hoverRight, 'translate-x-52': props.players.filter(el => !state.elemLeft.includes(el._id) && !state.elemRight.includes(el._id)).length > 0 }"
+                    class="element-right"
+                    @click="TryHoverRight"
+                >
+                    <PlusIcon class="h-20 w-20 my-auto mr-auto ml-12" />
+                    <div
+                        v-if="state.hoverRight"
+                        class="flex flex-col absolute z-50 text-sm text-black font-sansserif font-semibold bg-white w-40 rounded border border-gray/40 p-3 shadow-md right-[110%] top-[50%] -translate-y-[50%] after:absolute after:content-[''] after:h-4 after:w-4 after:-rotate-45 after:-right-[0.54rem] after:top-[50%] after:-translate-y-[50%] after:border-r after:border-b after:rounded-br after:border-gray/40 after:bg-white">
+                        <div
+                            v-for="player in props.players.filter(el => !state.elemLeft.includes(el._id) && !state.elemRight.includes(el._id))"
+                            :key="player._id"
+                            class="w-full rounded hover:bg-slate-200"
+                            @click="state.elemRight.push(player._id)">
                             <span class="ml-2">{{ player.name }}</span>
                         </div>
                     </div>
@@ -126,10 +160,26 @@ const onClickPlayer = async (id: string) => {
 }
 
 .element-left {
-  @apply relative flex h-36 bg-slate-50 cursor-pointer rounded;
+  @apply relative flex h-36 bg-slate-50 cursor-pointer rounded transition-transform -left-72 hover:translate-x-72;
 }
 
 .element-right {
-  @apply relative flex h-36 bg-slate-50 cursor-pointer rounded;
+  @apply relative flex h-36 bg-slate-50 cursor-pointer rounded transition-transform -right-72 hover:-translate-x-72;
+}
+
+.change-sheet-enter-active {
+  @apply delay-150 transition-all duration-500 ease-out;
+}
+
+.change-sheet-leave-active {
+  @apply transition-all duration-150 ease-in;
+}
+
+.change-sheet-enter-from {
+  transform: rotate3d(0, 1, 0, 90deg);
+}
+
+.change-sheet-leave-to {
+  transform: rotate3d(0, 1, 0, 90deg);
 }
 </style>
