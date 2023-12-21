4da1c15b2ce26dbaffa2c7e5d8c5a3f64f9f7d31
Troussierj13 <troussier.julien@gmail.com>
2023-12-21 18:33:10 +0100

    .

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import router from "@/router/router";
+import { createApp } from "vue";
 import App from "./App.vue";
 import "./styles/style.css";
-
-const { createApp, ref } = Vue
 
 createApp(App).use(router).mount("#app");
